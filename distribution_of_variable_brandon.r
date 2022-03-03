0d0541dd813b9530f21e76298fe948b9e7ac9796
brandonawds <bzl@uw.edu>
2022-03-02 21:37:55 -0800

    histogram update

--- distribution_of_variable_brandon.r
+++ distribution_of_variable_brandon.r
@@ -8,8 +8,6 @@ depressive_disorder <- mental_health_data %>%
   select(State, Value) %>%
   arrange(Value)
 
-bins <- seq(0, 40, by=5)
 depressive_disorder_indicators <- depressive_disorder$Value
-depressive_bins <- cut(depressive_disorder_indicators, bins)
 
-indicators_histogram <- hist(depressive_disorder_indicators, breaks=bins, main="Indicators of Depressive Disorder in the US", xlab="Indicators of Depressive Disorders")
+hist(depressive_disorder_indicators,main="Indicators of Depressive Disorder in the US", xlab="Indicators of Depressive Disorders")
