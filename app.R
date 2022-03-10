dea3ce7e46a635a3c6a7bddec1676d8f48696131
brandonawds <bzl@uw.edu>
2022-03-10 15:14:57 -0800

    overview

--- app.R
+++ app.R
@@ -11,16 +11,23 @@ library(shiny)
 source("brandon.R")
 source("fatin.R")
 # Define UI for application that draws a histogram
-ui <- navbarPage("Final Project",
+ui <- navbarPage("INFO 478 Final Project: Mental Health and Covid-19",
+      tabPanel("Overview",
+                titlePanel("Mental Health and Covid-19"),
+                h1("By: Brandon Ly, Fatin Almaroof, Tiffany Tse, Meena Attringal"),
+                h3("The purpose of our project is to examine the ways in which COVID-19 and the pandemic has affected the health of individuals in the United States.
+                   We will analyze data from the CDC and various other resources that provide us data on the rate of COVID-19 cases and the reported signs/symptoms of 
+                   various mental health disorders such as depression and anxeity. The question we hope to answer is: to what extend has COVID-19 impacted the mental 
+                   health of adults in the Unites States?")
+                 ),
     tabPanel("Mental Health of Disabled People and Covid-19",
              titlePanel("Depression and Anxiety Indicators by Disability Status"),
-             
              sidebarLayout(
                sidebarPanel(
                  selectInput("disorder", "Mental Health Disorder:",
                              c("Depression" = "depression",
                                "Anxiety" = "anxiety")),
-                 radioButtons("disabilityDemo", "Disability Demographic",
+                 radioButtons("disabilityDemo", "Disability Status:",
                               c("With Disability", "Without Disability", "Both")),
                ),
                
@@ -29,14 +36,22 @@ ui <- navbarPage("Final Project",
                )
              )),
     tabPanel("Mental Health Through the Phases of the Pandemic",
-             titlePanel("Fatin's Page"),
+             titlePanel("Mental Health Through the Phases of the Pandemic"),
              h1("Phase Comparison Plot"),
              sliderInput(inputId = "phases",
                          label = "Choose Phases to Display",
                          min = 1, max = 6, value = 1),
              h3("Phase Time Periods"),
              tableOutput("table"),
-             plotOutput("plot"))
+             plotOutput("plot")
+             ),
+    tabPanel("Conclusion and Insights",
+             titlePanel("Conclusions and Insights"),
+             h1("Mental Health of Disabled People in a Pandemic"),
+             h2("ANALYSIS"),
+             h1("Mental Health Through the Phases of the Pandemic"),
+             h2("ANALYSIS")
+             )
     
 )
 
